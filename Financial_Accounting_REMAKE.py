7eaaeeaeb3d349317f1f57ca7a9bddeefcbaa1aa
J. Brandon Walker <88942620+walkerjb02@users.noreply.github.com>
2022-02-02 10:59:26 -0500

    Update Financial_Accounting_REMAKE.py

--- Financial_Accounting_REMAKE.py
+++ Financial_Accounting_REMAKE.py
@@ -3,13 +3,11 @@ from Storage import jour
 class Main():
     def startup(self):
         while 1:
-            check, menu = input('What would you like to do?\n'), {'journal': 'Main().journal()',
-                                                                  'trial balance': 'Main().TrialBalance()',
-                                                                  'income': 'Main().IncomeAndRetainedEarnings()',
-                                                                  'balance': 'Main().BalanceSheet()',
-                                                                  'cash': 'Main().CashFlowsStatement()'}
+            check, menu = input('What would you like to do?\n'), {'journal': 'Main().journal()', 'trial balance': 'Main().TrialBalance()', 'incomeret': ['Income_Statement.csv', 'Retained_Earnings.csv'], 'balance': 'Balance_Sheet.csv', 'cash': 'Cash_Flows.csv'}
             for i in menu.keys():
                 if check in i:
+                    if 'incomeret' in check or 'balance' in check or 'cash' in check:
+                        self.acctgcycle = menu[check]
                     print(exec(menu[check]))
     def journal(self):
         with open('Journal.csv', 'a') as journalfile:
@@ -55,20 +53,13 @@ class Main():
             print(Main().journal())
             print(Main().TrialBalance())
         else: pass
-                
     def Statements(self):
-        acctgcycle = ['Income_Statement.csv', 'Retained_Earnings.csv', 'Balance_Sheet.csv', 'Cash_Flows.csv']
-        for a in acctgcycle:
+        for a in self.acctgcycle:
             with open(f'{a}', 'w') as Statement:
-                companyname, revenues, expenses, revenueamounts, expenseamounts, dividends = input('Insert Company Name Here: '), [
-                    i[1::] for i in jour.keys() if
-                    'revenue' in i or 'sales' in i or 'profit' in i or 'operating' in i], [j[1::] for j in jour.keys() if
-                                                                                           'taxes' in j or 'expense' in j or 'cost' in j], [
-                                                                                      jour[i] for i in jour.keys() if
-                                                                                      'revenue' in i or 'sales' in i or 'profit' in i or 'operating' in i], [
-                                                                                      jour[j] for j in jour.keys() if
-                                                                                      'taxes' in j or 'expense' in j or 'cost' in j], [sum(jour[i]) for i in jour.keys() if 'dividend' in jour.keys()]
-                net = sum(revenueamounts) - sum(expenseamounts)
+                companyname, revenues, expenses, revenueamounts, expenseamounts, dividendsl = input('Insert Company Name Here: '), [
+                    i[1::] for i in jour.keys() if 'revenue' in i or 'sales' in i or 'profit' in i or 'operating' in i], [j[1::] for j in jour.keys() if 'taxes' in j or 'expense' in j or 'cost' in j], [jour[i] for i in jour.keys() if 'revenue' in i or 'sales' in i or 'profit' in i or 'operating' in i], [jour[j] for j in jour.keys() if 'taxes' in j or 'expense' in j or 'cost' in j], [sum(jour[i]) for i in jour.keys() if 'dividend' in jour.keys()]
+                net, dividends = sum(revenueamounts) - sum(expenseamounts), sum(dividendsl)
+                retearn = net - dividends
                 if 'Income_Statement.csv' in a:
                     Statement.write(f"""Consolidated Income Statement for {companyname}\n\n\n Revenues\n""")
                     for b, c in revenues, revenueamounts:
@@ -77,12 +68,25 @@ class Main():
                     for e, f in expenses, expenseamounts:
                         Statement.write(f',{e},,{f}\n')
                     Statement.write(f'Net Income:,,,{net}')
+                    wb.open('Income_Statement.csv')
                 elif 'Retained_Earnings.csv' in a:
-                    Statement.write(f"""Statement of Retained Earnings for {companyname}\n\n\n Net Income,,,{net}\n{}""")
+                    Statement.write(f"""Statement of Retained Earnings for {companyname}\n\n\nNet Income,,,{net}\n,Dividends,,{dividends}\nRetained Earnings,,,{retearn}""")
+                    wb.open('Retained_Earnings.csv')
                 elif 'Balance_Sheet.csv' in a:
-                    pass
+                    Statement.write(f"""Balance Sheet for {companyname}\n\n\nAssets:""")
+                    for i in jour.keys():
+                        if str(i).startswith('d') or 'depreciation' in i:
+                            Statement.write(f',{i[1::]},,{jour[i]}\n')
+                        elif str(i).startswith('c') and not 'depreciation' in i: # have to fix the depreciation thing
+                            Statement.write('Liabilities and Shareholder Equity:\n\n')
+                            if 'payable' in i or 'debt' in i:
+                                Statement.write(f'{i[1::]},,,,{jour[i]}\n')
+                            if 'stock' in i:
+                                Statement.write(f'{i[1::]},,,,{jour[i]}\n')
+                    Statement.write(f"""Retained Earnings,,,,{retearn}\nTotal Assets and Liabilitites + Equity,,,{sum(jour[str(i).startswith('d') or 'depreciation' in i])},{sum(jour[str(i).startswith('c') and not 'depreciation' in i])}""")
+                    wb.open('Balance_Sheet.csv')
                 else:
-                    pass
+                    Statement.write(f'Cash Flows Statement for {companyname} -- Direct Method\n')
                 break
-            
+
 Main().startup()
